dd9afae44c5d70bfbb3d823295979762bfe135fa
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-09-30 22:10:54 +0200

    set mem to 8gb

--- sbatch/generate-synopsis-sbatch.py
+++ sbatch/generate-synopsis-sbatch.py
@@ -26,7 +26,7 @@ for trial_id in range(n_trials):
         f.write("#SBATCH --tasks-per-node=1\n")
         f.write("#SBATCH --cpus-per-task=1\n")
         f.write("#SBATCH --time=0:05:00\n")
-        f.write("#SBATCH --mem=4GB\n")
+        f.write("#SBATCH --mem=8GB\n")
         f.write("#SBATCH --output=" +\
             "../slurm/" + job_name + "_%j.out\n")
         f.write("\n")
